fa3901d329025ea8cce443cd548c920f76f1b5a9
Preeti Duhan <prtduhan@gmail.com>
2019-07-18 04:47:53 -0400

    Create reverse_words_string.py

---
+++ reverse_words_string.py
@@ -0,0 +1,12 @@
+'''
+Let the input string be “i like this program very much”. 
+The function should change the string to “much very program this like i”
+'''
+
+def reverse_string(sent):
+    l= sent.split()
+    #print(" ".join(reversed(l)))
+    
+reverse_string("i love storage")
+
+     
