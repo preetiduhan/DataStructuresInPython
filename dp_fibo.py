bf0efc0f51bec09f8b00514f5fdb5518ce792bb0
Preeti Duhan <prtduhan@gmail.com>
2020-03-30 08:13:52 +0530

    Create dp_fibo.py

---
+++ dp_fibo.py
@@ -0,0 +1,13 @@
+def fibo(n):
+    fib=[0]*(n+1)
+    fib[0]=0
+    fib[1]=1
+    for i in range(2,n+1):
+        fib[i]=fib[i-1]+fib[i-2]
+    print(fib[-1])
+fibo(9)
+    
+        
+    
+    
+    
